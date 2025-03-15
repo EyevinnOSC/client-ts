5d8c1302879f2dc09f189433d40620f6ad03efb4
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-15 09:47:06 +0100

    fix: missing export

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -2,4 +2,4 @@
 
 export { publish } from './publish';
 export { createCloudfrontDistribution } from './cdn';
-export { generateCommonAccessToken } from './cat';
+export { generateCommonAccessToken, validateCommonAccessToken } from './cat';
